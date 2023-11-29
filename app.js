9a722223ad92faabd53ff35cbd729318973637a3
ferat.goksu <ferat.goksu@ibb.gov.tr>
2023-11-29 11:15:06 +0300

    cleanBlog-Hw2

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
-
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
 
-  const blog = {
-    id: 1,
-    name: "Blog Title",
-    description: "Blog description"
-  }
-  res.send(blog)
-})
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about')
+
+
+});
+app.get('/add_post', (req, res) => {
+
+  res.render('add_post');
+});
 
 const port = 3000;
 app.listen(port, () => {
